792889b304cf82455a0562c3fcf91a81d2e44997
Anatoly Savin <a.savin1@g.nsu.ru>
2024-09-13 02:00:37 +0700

    fix

--- 22216/a.savin1/lab19/lab19.c
+++ 22216/a.savin1/lab19/lab19.c
@@ -1,64 +1,58 @@
+#define  _FILE_OFFSET_BITS 64
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <fnmatch.h>
-#include <unistd.h>
 #include <errno.h>
 
-
-int main() {
-    errno = 0;
-    long name_max = pathconf(".", _PC_NAME_MAX);
-    if (name_max == -1) {
-	if (errno != 0) {
-            perror("failed to get max length of filename");
-            return -1;
-	} else {
-	    name_max = 255;
-	    fprintf(stderr, "name_max is not defined for this fyle system, using default 255\n");
-	}
+void closedir_or_exit(DIR *dir) {
+    if (closedir(dir) == -1) {
+	perror("closedir failed");
+	exit(-1);
     }
-    char pattern[name_max + 1];
+}
 
-    printf("enter a template for the filename: ");
-    if (fgets(pattern, sizeof(pattern), stdin) == NULL) {
-        perror("fgets failed");
-        return -1;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s <pattern>\n", argv[0]);
+	return -1;
     }
 
-    size_t len = strlen(pattern);
-    if (len > 0 && pattern[len - 1] == '\n') {
-        pattern[len - 1] = '\0';
+    if (strchr(argv[1], '/') != NULL) {
+	fprintf(stderr, "pattern can not contain /\n");
+	return -1;
     }
 
-    if (strchr(pattern, '/') != NULL) {
-        fprintf(stderr, "pattern can not contain /\n");
-        return -1;
-    }
-
-    struct dirent *entry;
     DIR *dir;
 
     dir = opendir(".");
     if (dir == NULL) {
-        perror("opendir failed");
-        return -1;
+ 	perror("opendir failed");
+	return -1;
     }
 
     int found = 0;
+    struct dirent *entry;
 
     while ((entry = readdir(dir)) != NULL) {
-        if (fnmatch(pattern, entry->d_name, 0) == 0) {
-            printf("%s\n", entry->d_name);
-            found = 1;
-        }
+	if (fnmatch(argv[1], entry->d_name, 0) == 0) {
+	    printf("%s\n", entry->d_name);
+	    found = 1;
+	}
     }
-    closedir(dir);
+
+    if (errno != 0) {
+   	perror("readdir failed");
+  	closedir_or_exit(dir);
+ 	return -1;
+    }
 
     if (!found) {
-        printf("failed to find files matching the template: %s\n", pattern);
+	printf("failed to find files matching the template: %s\n", argv[1]);
     }
 
+    closedir_or_exit(dir);
     return 0;
 }
